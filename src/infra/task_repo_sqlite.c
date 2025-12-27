44e8341b538e487d6f95593570fc99eaaf1f543f
Adriano Garcia Silveira <a.gsilveira@outlook.com>
2025-12-27 19:17:56 -0300

    feat: add defintion for ensure_schema function

--- src/infra/task_repo_sqlite.c
+++ src/infra/task_repo_sqlite.c
@@ -23,7 +23,7 @@ static char *xstrdup_local(const char *s)
 static int exec_sql(sqlite3 *db, const char *sql)
 {
     char *errmsg = NULL;
-    int rc = sqlite3_exec(db, sql, NULL, NULL, &errmsg);
+    int rc = sqlite3_exec(db, sql, NULL, NULL, &errmsg); // TODO: how can we prevent sql injection here?
     if (rc != SQLITE_OK)
     {
         if (errmsg)
@@ -35,3 +35,13 @@ static int exec_sql(sqlite3 *db, const char *sql)
     return 1;
 }
 
+static int ensure_schema(sqlite3 *db)
+{
+    const char *sql =
+        "CREATE TABLE IF NOT EXISTS tasks ("
+        " id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        " text TEXT NOT NULL,"
+        " completed INTEGER NOT NULL CHECK(completed IN (O,1))"
+        ");";
+    return exec_sql(db, sql);
+}

77babc88c294df74744555e5402652ad8454bfda
Adriano Garcia Silveira <a.gsilveira@outlook.com>
2025-12-27 16:41:11 -0300

    feat: add typedef of task

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Task
+{
+  char *task;
+  int completed; // 1 for completed, 0 for not completed
+};
+
+int main()
+{
+  return 0;
+}

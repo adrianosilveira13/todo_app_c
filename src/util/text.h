a0db82c0b194051e11f0c0a126eedff578281429
Adriano Garcia Silveira <a.gsilveira@outlook.com>
2025-12-28 13:01:22 -0300

    feat: add more definitions to text header file

--- src/util/text.h
+++ src/util/text.h
@@ -1,6 +1,14 @@
 #ifndef TEXT_H
 #define TEXT_H
 
+#include <stddef.h>
+
+char *text_read_line(const char *prompt); // malloc
+int text_read_int_range(const char *prompt, int min, int max, int *out);
+
+char *text_encode_one_line(const char *raw); // malloc
+char *text_decode_one_line(const char *enc); // malloc
+
 /**
  * Checks if the given needle is a substring of haystack, case-insensitive.
  */
